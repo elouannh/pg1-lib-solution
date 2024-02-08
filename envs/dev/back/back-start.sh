b698a6e0f6f2a2450d5fd2288f50e226fd7eac35
elouannh <elouannhosta01@gmail.com>
2024-02-08 17:44:06 +0100

    Rename the project

--- envs/dev/back/back-start.sh
+++ envs/dev/back/back-start.sh
@@ -3,8 +3,8 @@
 all_missing_env_vars=()
 check_env_var() {
   var_value=$(printf '%s\n' "${!1}")
-  if [ -z $var_value ]; then
-    all_missing_env_vars+=( $1 )
+  if [ -z "$var_value" ]; then
+    all_missing_env_vars+=( "$1" )
   fi
 }
 check_env_var DB_USER
@@ -13,19 +13,16 @@ check_env_var DB_IP
 check_env_var DB_PORT
 
 if [ ${#all_missing_env_vars[@]} -ne 0 ]; then
-    echo "Missing environment variables: $all_missing_env_vars"
+    echo "Missing environment variables: ${all_missing_env_vars[@]}"
     exit 1
 fi
 
-pg_isready -d ${DB_NAME} -h ${DB_IP} -p ${DB_PORT} -U ${DB_USER}
-while [[ $? -ne 0 ]] ; do
+while ! pg_isready -d "${DB_NAME}" -h "${DB_IP}" -p "${DB_PORT}" -U "${DB_USER}" -t 1; do
   echo "Waiting for postgres server ready to accept connections..."
   sleep 2
-  pg_isready -d ${DB_NAME} -h ${DB_IP} -p ${DB_PORT} -U ${DB_USER}
 done
 
 echo "Postgres server ready to accept connections, starting flask server"
-
 
 export FLASK_APP=main.py
 
