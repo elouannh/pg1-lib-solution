9d1031f261b5983b97566756bfa5314d6b4a71d1
elouannh <elouannhosta01@gmail.com>
2024-02-10 19:15:22 +0100

    fixed the imports

--- app/data/auth/schemas/auth_schema.py
+++ app/data/auth/schemas/auth_schema.py
@@ -1,6 +1,6 @@
 """Schema for serializing/deserializing a BorrowBookModel"""
 
-from data.book.models.auth_model import AuthModel
+from data.auth.models.auth_model import AuthModel
 from shared.utils.schema.base_schema import BaseSchema
 
 
